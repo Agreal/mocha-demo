1a4afc89bd36bf1f183a6bc60e37d95e4651ab29
Qin Xiaoqiang <qxq111@gmail.com>
2017-06-19 23:00:05 +0800

    stub test: wrapper callback method with Promise

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
-exports.sum = function sum(a, b) {
-    return a + b;
+global.getUserByIdOld = function getUserByIdOld(id, successFn, failureFn) {
+    setTimeout(function() {
+        successFn({name: 'agreal'});
+    }, 500);
+}
+
+exports.getUserById = function getUserById(id) {
+    return new Promise(function(resolve, reject) {
+        getUserByIdOld(id,
+            function(user) {
+                resolve(user);
+            },
+            reject);
+    });
 }
--- src/main.test.js
+++ src/main.test.js
@@ -1,8 +1,53 @@
 const main = require('./main.js');
 
-describe('sum', function() {
-  it('should display right text content', function() {
-    var result = main.sum(1, 1)
-    expect(result).to.equal(2);
+describe('getUserById', () => {
+  let stubGetUserByIdOld;
+  beforeEach(() => {
+    stubGetUserByIdOld = sinon.stub(global, 'getUserByIdOld');
+  });
+  afterEach(() => {
+    stubGetUserByIdOld.restore();
+  });
+
+  it('stub', () => {
+    const successFn = sinon.stub().returns({name: 'agreal1'});
+    expect(successFn().name).to.equal('agreal1');
+
+    const failureFn = sinon.stub().returns('Error: name is agreal');
+    expect(failureFn()).to.equal('Error: name is agreal');
+  });
+
+  it('should call success', function(done) {
+    const successFn = sinon.stub().returns({name: 'agreal1'});
+
+    stubGetUserByIdOld.callsFake(function(id, successFakeFn) {
+      expect(id).to.equal(1);
+      successFakeFn(successFn());
+    });
+
+    main.getUserById(1)
+      .then((user) => {
+        expect(user.name).to.equal('agreal1');
+      }, (errorMessage) => {
+        expect(errorMessage).to.equal('Error: name is agreal1');
+      })
+      .then(done);
+  });
+
+  it('should call failure', function(done) {
+    const failureFn = sinon.stub().returns('Error: name is agreal2');
+
+    stubGetUserByIdOld.callsFake(function(id, successFakeFn, failureFakeFn) {
+      expect(id).to.equal(1);
+      failureFakeFn(failureFn());
+    });
+
+    main.getUserById(1)
+      .then((user) => {
+        expect(user.name).to.equal('agreal2');
+      }, (errorMessage) => {
+        expect(errorMessage).to.equal('Error: name is agreal2');
+      })
+      .then(done);
   });
 });
