85b3a4546174b05327ad3cc59fd841e39c1cf998
Qin Xiaoqiang <qxq111@gmail.com>
2017-06-21 19:05:28 +0800

    inline stub data

--- src/main.test.js
+++ src/main.test.js
@@ -9,20 +9,10 @@ describe('getUserById', () => {
     stubGetUserByIdOld.restore();
   });
 
-  it('stub', () => {
-    const successFn = sinon.stub().returns({name: 'agreal1'});
-    expect(successFn().name).to.equal('agreal1');
-
-    const failureFn = sinon.stub().returns('Error: name is agreal');
-    expect(failureFn()).to.equal('Error: name is agreal');
-  });
-
   it('should call success', (done) => {
-    const successFn = sinon.stub().returns({name: 'agreal1'});
-
     stubGetUserByIdOld.callsFake((id, successFakeFn) => {
       expect(id).to.equal(1);
-      successFakeFn(successFn());
+      successFakeFn({name: 'agreal1'});
     });
 
     main.getUserById(1)
@@ -35,11 +25,9 @@ describe('getUserById', () => {
   });
 
   it('should call failure', (done) => {
-    const failureFn = sinon.stub().returns('Error: name is agreal2');
-
     stubGetUserByIdOld.callsFake((id, successFakeFn, failureFakeFn) => {
       expect(id).to.equal(1);
-      failureFakeFn(failureFn());
+      failureFakeFn('Error: name is agreal2');
     });
 
     main.getUserById(1)
